75a68e468052a3277dd56d965d5bc631af3eb790
ec4016 <112779974+ec4016@users.noreply.github.com>
2023-03-01 20:25:58 -0500

    Visual Updates
    - UI
    - Background
    - Camera
    - Player health and dmg

--- Exercise 5 - Adventure Game/Assets/Scripts/GameManager.cs
+++ Exercise 5 - Adventure Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int health = 3;
+    public static bool pause = false;
+
+    public TextMeshProUGUI healthUI;
+    public GameObject pauseUI;
+
+    private void Start()
     {
-        
+        healthUI.text = "LIVES: " + health;
+        pauseUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HealthDecr(int points) {
+        health -= points;
+        healthUI.text = "LIVES: " + health;
+        if(health <= 0)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+    public void Resume() {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        pause = false;
+    }
+
+    public void Pause() {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        pause = true;
+    }
+
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (pause) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public int GetHealth()
+    {
+        return health;
     }
 }

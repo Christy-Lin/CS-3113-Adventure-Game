75be9643803969ab7c732620f2545228082f8e79
Christy-Lin <cl5501@nyu.edu>
2023-03-08 17:18:42 -0500

    Completed puzzleRun scene

--- Exercise 5 - Adventure Game/Assets/Scripts/Chaser.cs
+++ Exercise 5 - Adventure Game/Assets/Scripts/Chaser.cs
@@ -9,15 +9,13 @@ public class Chaser : MonoBehaviour
     GameObject player;
     GameManager _gameManager;
     int keyTaken;
+    float chaserSpeed;
+    public bool isStopped;
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
         _gameManager = GameObject.FindObjectOfType<GameManager>();
-        
-        
-
-        StartCoroutine(ChasePlayer());
     }
 
 
@@ -25,15 +23,19 @@ public class Chaser : MonoBehaviour
     {
         keyTaken = _gameManager.getKeys();
         print(keyTaken);
+        StartCoroutine(ChasePlayer());
     }
 
     IEnumerator ChasePlayer() {
-        while (true) {
-
-            // if (keyTaken == 1) {
+        if (true) {
+            if (keyTaken % 2 == 1) {
+                _navMeshAgent.isStopped = false;
                 yield return new WaitForSeconds(1f);
                 _navMeshAgent.destination = player.transform.position;
-            // }
+            } else {
+                _navMeshAgent.isStopped = true;
+                _navMeshAgent.ResetPath();
+            }
         }
     }
 

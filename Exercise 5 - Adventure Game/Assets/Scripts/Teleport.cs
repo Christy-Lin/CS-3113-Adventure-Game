7fd3026768dd9b5f149160e422c8f458abc46528
Nishma <ns4606@nyu.edu>
2023-03-09 00:37:19 -0500

    puzzleChoose
    kind of a guessing game (4 rooms, for each you need to choose between 2 doors to get to the next)
    added teleporting

---
+++ Exercise 5 - Adventure Game/Assets/Scripts/Teleport.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    GameObject player;
+    NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // _navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        _navMeshAgent = player.GetComponent<NavMeshAgent>();    // this is the player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            // FIRST, check if the name of self... like gemeObject.name??
+            // if it's Door (6), then load next scene:
+            
+            // OTHERWISE, teleport:
+            _navMeshAgent.Warp(teleportTarget.transform.position);
+        }
+
+
+    }
+
+
+
+
+}
